e9c2e469cf8bfaa101aa543d1a59afd6dbcc71c4
pranitalohkare00 <pranitalohkare7772@gmail.com>
2025-05-11 09:18:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Basic_Opration
+{
+    internal class Pragram
+    {
+        static void Main(string[] args)
+        {
+            int lintA, lintB, lintC;
+
+            Console.WriteLine("Enter value for a");
+            lintA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for b");
+            lintB= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for c");
+            lintC = Convert.ToInt32(Console.ReadLine());
+
+            int lintSum = SimpleCode.Add(lintA, lintB);
+            lintSum = SimpleCode.Add(lintSum, lintC);
+            int lintSub = SimpleCode.Subtraction(lintA, lintB);
+            int lintMultiply = SimpleCode.Multiply(lintA, lintB); 
+            float lintDiv = SimpleCode.Division(lintA, lintB);
+            Console.WriteLine("Addition= " + lintSum);
+            Console.WriteLine("Subtraction= " + lintSub);
+            Console.WriteLine("Muitiplication= " + lintMultiply);
+            Console.WriteLine("Division= " + lintDiv);
+            
+        }
+        
+    }
+}
---
+++ SimpleCode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Opration
+{
+    internal class SimpleCode
+    {
+  
+            public static int Add(int a, int b)
+            {
+                return a + b;
+            }
+
+            public static int Subtraction(int a, int b)
+            {
+                return a - b;
+            }
+            public static int Multiply(int a, int b)
+            {
+                return a * b;
+            }
+
+            public static float Division(int a, int b)
+            {
+                return (float)a / b;
+            }
+
+        
+
+    }
+}
+
